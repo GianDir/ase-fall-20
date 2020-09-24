aad5b44321f528cb18ee5e46cd1f881cde82aea6
Gianmaria <g.dirito@studenti.unipi.it>
2020-09-24 17:53:08 +0200

    first commit

---
+++ Lab_2/FooCalculator.py
@@ -0,0 +1,14 @@
+import Calculator as c 
+
+class FooCalculator:
+
+    #empty constructor
+    def __init__(self):
+        pass
+
+    def sum(self, m, n):
+        return c.sum(m, n)
+    
+    def divide(self, m, n):
+        return c.divide(m, n)
+    
